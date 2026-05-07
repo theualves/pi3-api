572d87d4fee2ed00c91c31a263046ea8c53d5ac3
Maria Cecília <cecilmari33@gmail.com>
2026-05-07 20:55:17 -0300

    correção do arquivo de routes

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import { criarUsuario, cadastrarAluno, listarUsuarios, contarUsuarios, recuperarSenha } from '../controllers/UsuarioController.js';
+import { criarUsuario, cadastrarAluno, listarUsuarios, contarUsuarios } from '../controllers/UsuarioController.js';
 
 const router = express.Router();
 
 router.get('/', listarUsuarios);
 router.post('/', criarUsuario);
 router.post("/aluno", cadastrarAluno);
-router.post('/recuperar-senha', recuperarSenha);
 router.get("/contar", contarUsuarios);
 
 export default router;
