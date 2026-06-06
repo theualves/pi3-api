8890559499b2d4500753a6be5df927679ef3f9f5
Matheus Alves <matheusalves2906@gmail.com>
2026-06-06 11:52:42 -0300

    feat:adicionadas rotas de editarAluno e excluirAluno

--- src/routes/alunoCoordenadorRoutes.js
+++ src/routes/alunoCoordenadorRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { criarAluno, listarAlunos, gerarSenhaAutomatica } from "../controllers/AlunoCoordenadorController.js";
+import { criarAluno, listarAlunos, gerarSenhaAutomatica, atualizarAluno, excluirAluno} from "../controllers/AlunoCoordenadorController.js";
 
 const router = express.Router();
 
 router.post('/', criarAluno);
 router.get('/', listarAlunos);
+router.put('/alunos/:id', atualizarAluno);
+router.delete('/alunos/:id', excluirAluno);
 router.get('/senha/automatica', gerarSenhaAutomatica);
 
 export default router;
