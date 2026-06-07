8895a829c6ddd316d0840a01b9bfb69cb3b90ef0
Matheus Alves <matheusalves2906@gmail.com>
2026-06-06 22:55:53 -0300

    feat: rotas de atualizar/excluir turma adicionado

--- src/routes/turmaRoutes.js
+++ src/routes/turmaRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { criarTurma, listarTurmas } from "../controllers/TurmaController.js";
+import { criarTurma, listarTurmas, atualizarTurma, excluirTurma} from "../controllers/TurmaController.js";
 
 const router = express.Router();
 router.post('/', criarTurma);
 router.get('/', listarTurmas);
+router.put('/:id', atualizarTurma);
+router.delete('/:id', excluirTurma);
 
 export default router;
